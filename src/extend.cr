202f9eb1d41131a99d72eeda62ec088a2d20ebf8
marka <>
2026-02-04 00:34:34 +0000

    Add help command.
    FossilOrigin-Name: 94cc6b0b1bd23e9d69e5fe668e9f453a49ad6002438c3a0be7e6f1c79eec309e

--- src/extend.cr
+++ src/extend.cr
@@ -3,6 +3,7 @@ module Extend
 
   extend self
 
+  # Prompts the user for a command name, and runs that command.
   def extendedcommand(f : Bool, n : Int32, k : Int32) : Result
     result, name = Echo.reply_with_completions(": ", nil, true) do |s|
       # Find all command names that start with s.
@@ -15,9 +16,23 @@ module Extend
     return E.keymap.call_by_name(name, f, n, k)
   end
 
+  # Waits for the user to hit a key, then shows the name of the
+  # command bound to that key
+  def help(f : Bool, n : Int32, k : Int32) : Result
+    k = E.kbd.getkey
+    name = E.keymap.k2n[k]?
+    if name
+      Echo.puts("[#{Kbd.keyname(k)} is bound to #{name}]")
+    else
+      Echo.puts("[#{Kbd.keyname(k)} is unbound]")
+    end
+    return TRUE
+  end
+
     # Creates key bindings for all Extend commands.
   def bind_keys(k : KeyMap)
     k.add(Kbd.meta('x'), cmdptr(extendedcommand), "extended-command")
+    k.add(Kbd::F1, cmdptr(help), "help")
   end
 
 end
